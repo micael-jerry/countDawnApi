e32723cc07139813ebc506d7d40e94bcf06c59cf
Micael Jerry <hei.jerry.2@gmail.com>
2022-08-12 11:09:45 +0300

    add note

---
+++ src/main/java/com/example/helloworldapi/model/CandidatNote.java
@@ -0,0 +1,4 @@
+package com.example.helloworldapi.model;
+
+public class CandidatNote {
+}
--- src/main/java/com/example/helloworldapi/model/Note.java
+++ src/main/java/com/example/helloworldapi/model/Note.java
@@ -1,4 +1,28 @@
 package com.example.helloworldapi.model;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "notes")
 public class Note {
+    @Id
+    private int id;
+    @Column(name = "math_concours")
+    private float mathConcours;
+    @Column(name = "frs_concours")
+    private float frsConcours;
+    @Column(name = "math_tob")
+    private float mathTOB;
+    @Column(name = "frs_tob")
+    private float frsTOB;
 }
