186538ef5f46b56a600a976e51541f70f0088a3c
Micael Jerry <hei.jerry.2@gmail.com>
2022-08-12 13:53:14 +0300

    add User model

--- src/main/java/com/example/helloworldapi/model/User.java
+++ src/main/java/com/example/helloworldapi/model/User.java
@@ -1,5 +1,23 @@
 package com.example.helloworldapi.model;
 
-public class User {
+import lombok.*;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+    @Column(nullable = false,length = 125)
+    private String nom;
+    private String prenom;
+    @Column(nullable = false,length = 100)
+    private String utilisateur;
 }
